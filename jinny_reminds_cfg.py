5d5dda1dd2fd7eb0a916f4bad5c2f7d89bb1c813
Daedalus <daedalus.chan@gmail.com>
2020-05-31 02:02:28 +0800

    updated python code to retrieve config value from env var

--- jinny_reminds_cfg.py
+++ jinny_reminds_cfg.py
@@ -1,10 +1,12 @@
-TOKEN = ""
-LIST_OF_ADMINS = []
-LIST_OF_SUPER_ADMINS = []
+import os
 
-db_host = ''
+TOKEN = os.environ['REMIND_TOKEN']
+LIST_OF_ADMINS = os.environ['ADMIN_LIST'].split(',')
+LIST_OF_SUPER_ADMINS = os.environ['SUPER_ADMIN']
+
+db_host = 'db-mongo'
 db_port = 27017
-db_user = ''
-db_pwd = ''
+db_user = os.environ['MONGO_USER']
+db_pwd = os.environ['MONGO_PWD']
 
 conn_str = 'mongodb://{}:{}@{}:{}/?authSource=admin'.format(db_user, db_pwd, db_host, db_port)
